92afde96d992df98b44cb1511517c15e6f33b6d1
Alejandro CR <contacto@alejandrocr.co>
2025-04-14 21:01:23 -0500

    feat: update TypeScript configuration and Vite setup for path aliasing

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
+import * as path from 'node:path';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     port: 3000,
     open: true,
