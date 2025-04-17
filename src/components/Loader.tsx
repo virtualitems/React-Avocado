446b9ac116b3b56c0887895e9e158a37a6e9db09
Alejandro CR <contacto@alejandrocr.co>
2025-04-16 23:36:24 -0500

    feat: add Loader component for loading state indication

---
+++ src/components/Loader.tsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <div className="fixed inset-0 flex justify-center items-center bg-white bg-opacity-75">
+      <div className="border-4 border-t-blue-500 border-blue-200 rounded-full w-12 h-12 animate-spin"></div>
+    </div>
+  );
+}
