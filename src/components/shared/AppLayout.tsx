55ddcd8b6c9741d9aea936510ea020e233ccb598
Alejandro CR <contacto@alejandrocr.co>
2025-04-16 20:50:16 -0500

    style: add styles to header and footer in AppLayout

--- src/components/shared/AppLayout.tsx
+++ src/components/shared/AppLayout.tsx
@@ -1,12 +1,15 @@
-import { type ReactElement } from 'react';
 import { Outlet } from 'react-router';
 
-export default function AppLayout(): ReactElement {
+export default function AppLayout(): React.ReactElement {
   return (
-    <div>
-      <header>Header</header>
-      <Outlet />
-      <footer>Footer</footer>;
+    <div className="relative min-h-screen">
+      <header className="bg-gray-800 p-4 text-white">Header</header>
+      <main className="pb-5">
+        <Outlet />
+      </main>
+      <footer className="bottom-0 left-0 absolute bg-gray-800 p-4 w-full text-white">
+        Footer
+      </footer>
     </div>
   );
 }
