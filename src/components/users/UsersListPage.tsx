bc1ad20febd10e46448ba31a7b22fc36d623af75
Alejandro CR <contacto@alejandrocr.co>
2025-04-16 20:50:58 -0500

    feat: implement UsersListPage component to display and manage users

---
+++ src/components/users/UsersListPage.tsx
@@ -0,0 +1,103 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router';
+
+type User = {
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+};
+
+export default function UsersListPage(): React.ReactElement {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    fetch('https://reqres.in/api/users')
+      .then((response) => {
+        if (response.ok === false)
+          throw new Error('Network response was not ok');
+        return response.json();
+      })
+      .then((json) => {
+        setUsers(json.data);
+      })
+      .catch((error) => {
+        console.warn(error);
+      });
+  }, []);
+
+  const onDelete = (_event: React.MouseEvent, index: number) => {
+    const user = users[index];
+    console.log('Delete user', user);
+  };
+
+  return (
+    <div className="bg-white mx-auto mt-4 p-4 rounded container">
+      <table className="w-full text-center">
+        <thead>
+          <tr className="bg-gray-200 pb-2">
+            <th>ID</th>
+            <th>Email</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Show</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <ListUsersRow
+              key={user.id}
+              index={index}
+              user={user}
+              onDelete={onDelete}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+function ListUsersRow(props: {
+  index: number;
+  user: { id: number; email: string; first_name: string; last_name: string };
+  onDelete?: (event: React.MouseEvent, index: number) => void;
+}) {
+  const { user, index, onDelete } = props;
+
+  return (
+    <tr className="hover:bg-gray-100">
+      <td className="py-2">{user.id}</td>
+      <td className="py-2">{user.email}</td>
+      <td className="py-2">{user.first_name}</td>
+      <td className="py-2">{user.last_name}</td>
+      <td className="py-2">
+        <Link
+          className="bg-blue-500 hover:bg-blue-700 active:opacity-75 px-4 py-2 rounded font-bold text-white cursor-pointer"
+          to={`/users/${user.id}`}
+        >
+          Show
+        </Link>
+      </td>
+      <td className="py-2">
+        <Link
+          className="bg-yellow-500 hover:bg-yellow-700 active:opacity-75 px-4 py-2 rounded font-bold text-white cursor-pointer"
+          to={`/users/${user.id}/edit`}
+        >
+          Edit
+        </Link>
+      </td>
+      <td className="py-2">
+        <button
+          className="bg-red-500 hover:bg-red-700 active:opacity-75 px-4 py-2 rounded font-bold text-white cursor-pointer"
+          onClick={(event) => onDelete?.(event, index)}
+        >
+          Delete
+        </button>
+      </td>
+    </tr>
+  );
+}
