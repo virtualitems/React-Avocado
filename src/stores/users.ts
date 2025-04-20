987ac15314b5f106c0d6299f921c328ab2af1a3e
Alejandro CR <contacto@alejandrocr.co>
2025-04-20 18:59:04 -0500

    feat: extend users context with add, update, and delete user methods

--- src/stores/users.ts
+++ src/stores/users.ts
@@ -3,7 +3,13 @@ import { createContext } from 'react';
 export const usersContext = createContext<{
   users: User[];
   setUsers: React.Dispatch<React.SetStateAction<User[]>>;
+  addUser: (user: User) => void;
+  updateUser: (index: number, user: User) => void;
+  deleteUser: (index: number) => void;
 }>({
   users: [],
   setUsers: () => {},
+  addUser: () => {},
+  updateUser: () => {},
+  deleteUser: () => {},
 });
