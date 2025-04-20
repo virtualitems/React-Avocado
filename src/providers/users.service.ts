30eabb15b01d06e17f314f8a3f6b7e13e2cf42fc
Alejandro CR <contacto@alejandrocr.co>
2025-04-20 18:58:49 -0500

    feat: implement user service with CRUD operations

---
+++ src/providers/users.service.ts
@@ -0,0 +1,96 @@
+const API_URL = 'https://reqres.in';
+
+export async function listUsers(page: number = 1, perPage: number = 6) {
+  const fetchTarget = new URL('/api/users', API_URL);
+  fetchTarget.search = `page=${page}&per_page=${perPage}`;
+
+  const fetchOptions: RequestInit = {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+    },
+  };
+
+  const response = await fetch(fetchTarget, fetchOptions);
+
+  if (response.ok === false) {
+    throw new Error(response.statusText);
+  }
+
+  return response;
+}
+
+export async function getUser(id: string) {
+  const fetchTarget = new URL(`/api/users/${id}`, API_URL);
+
+  const fetchOptions: RequestInit = {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+    },
+  };
+
+  const response = await fetch(fetchTarget, fetchOptions);
+
+  if (response.ok === false) {
+    throw new Error(response.statusText);
+  }
+
+  return response;
+}
+
+export async function createUser(formData: FormData) {
+  const fetchTarget = new URL('/api/users', API_URL);
+
+  const fetchOptions: RequestInit = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+    body: formData,
+  };
+
+  const response = await fetch(fetchTarget, fetchOptions);
+
+  if (response.ok === false) {
+    throw new Error(response.statusText);
+  }
+
+  return response;
+}
+
+export async function updateUser(id: string, formData: FormData) {
+  const fetchTarget = new URL(`/api/users/${id}`, API_URL);
+
+  const fetchOptions: RequestInit = {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+    body: formData,
+  };
+
+  const response = await fetch(fetchTarget, fetchOptions);
+
+  if (response.ok === false) {
+    throw new Error(response.statusText);
+  }
+
+  return response;
+}
+
+export async function deleteUser(id: string) {
+  const fetchTarget = new URL(`/api/users/${id}`, API_URL);
+
+  const fetchOptions: RequestInit = {
+    method: 'DELETE',
+  };
+
+  const response = await fetch(fetchTarget, fetchOptions);
+
+  if (response.ok === false) {
+    throw new Error(response.statusText);
+  }
+
+  return response;
+}
